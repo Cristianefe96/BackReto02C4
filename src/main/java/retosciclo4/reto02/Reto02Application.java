575fa2e951dd7f32d483dedd58e0cd4dbb1a8dcc
CRISTIAN FABIAN GOMEZ <cristianf.gomez.mt@correo.usa.edu.co>
2021-12-12 15:06:59 -0500

    first commit

---
+++ src/main/java/retosciclo4/reto02/Reto02Application.java
@@ -0,0 +1,30 @@
+package retosciclo4.reto02;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+// import retosciclo4.reto02.interfaces.InterfaceProducts;
+// import retosciclo4.reto02.interfaces.InterfaceUser;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
+
+// @Component
+@SpringBootApplication
+public class Reto02Application {
+	// @Autowired
+	// private InterfaceProducts interfaceProducts;
+
+	// @Autowired
+	// private InterfaceUser interfaceUser;
+	public static void main(String[] args) {
+		SpringApplication.run(Reto02Application.class, args);
+	}
+
+	// @Override
+	// public void run(String...args) throws Exception {
+	// 	interfaceProducts.deleteAll();
+	// 	interfaceUser.deleteAll();
+	// }
+
+}
